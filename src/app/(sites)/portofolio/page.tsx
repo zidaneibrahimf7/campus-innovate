a0f86f3956f2a15b73097fdeee5ccce10da9f613
shibgotalloh <shibgotalloh_qqq@apps.ipb.ac.id>
2024-10-27 09:38:11 +0700

    update: portofolio and about

--- src/app/(sites)/portofolio/page.tsx
+++ src/app/(sites)/portofolio/page.tsx
@@ -1,9 +1,64 @@
+import { CardDemo } from '@/components/custom/Cards/cardDemo';
+import Image from 'next/image';
+
 export default function PortofolioPage() {
+  const data = [
+    {
+      url: '/assets/images/cda_1.JPG',
+      category: 'CDA IPB',
+      date: 'March 02th, 2024',
+      name: 'Baris Berbaris',
+      description: 'Derap langkah nan gagah perkasa. Seirama dalam satu suara',
+    },
+    {
+      url: '/assets/images/cda_3.JPG',
+      category: 'CDA IPB',
+      date: 'March 02th, 2024',
+      name: 'Pose',
+      description: 'Kasih gaya terbaikmu.',
+    },
+    {
+      url: '/assets/images/cda_4.JPG',
+      category: 'CDA IPB',
+      date: 'March 02th, 2024',
+      name: 'Abadikan momen',
+      description: 'Kelak akan terkenang sepanjang masa',
+    },
+    {
+      url: '/assets/images/cda_5.JPG',
+      category: 'CDA IPB',
+      date: 'March 02th, 2024',
+      name: 'Keep Smile',
+      description: 'Senyum itu ibadah.',
+    },
+    {
+      url: '/assets/images/cda_6.JPG',
+      category: 'CDA IPB',
+      date: 'March 02th, 2024',
+      name: 'Teamwork',
+      description: 'Kerja sama untuk menyelesaikan masalah.',
+    },
+    {
+      url: '/assets/images/klhk_2.jpg',
+      category: 'KLHK',
+      date: 'July 22nd, 2023',
+      name: 'big W',
+      description: 'Jangan lupa berpose.',
+    },
+  ];
+
   return (
     <main>
-      <section className="py-[100px] px-[120px] flex flex-col gap-[60px]">
-        <h1 className="text-secondary font-bold capitalize text-[4rem]">portofolio</h1>
-        <main></main>
+      <section className="py-[100px] px-[120px] w-full flex flex-col justify-center gap-[60px]">
+        <h1 className="text-secondary text-center font-bold capitalize text-[4rem]">portofolio</h1>
+        <main>
+          <iframe className="rounded-lg " width="100%" height="610" src="/assets/images/testimoni.mp4" allow="autoplay" allowFullScreen></iframe>
+        </main>
+        <main className="grid grid-cols-2 gap-[24px]">
+          {data.map((res, index) => (
+            <CardDemo key={index} url={res.url} category={res.category} date={res.date} title={res.name} description={res.description} />
+          ))}
+        </main>
       </section>
     </main>
   );
--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -32,60 +32,67 @@ export default function Navbar() {
     },
   ];
   const [select, setSelect] = useState(0);
-  const router = useRouter()
-  const pathname = usePathname()
-  
+  const router = useRouter();
+  const pathname = usePathname();
+
   const handleSelect = (index: number) => {
     // setSelect(index)
-    switch(index){
+    switch (index) {
       case 0:
-        return router.push('/home')
+        return router.push('/home');
         break;
       case 1:
-        return router.push('/layanan')
+        return router.push('/layanan');
         break;
       case 2:
-        return router.push('/goals')
+        return router.push('/goals');
         break;
       case 3:
-        return router.push('/portofolio')
+        return router.push('/portofolio');
         break;
       case 4:
-        return router.push('/about')
+        return router.push('/about');
         break;
       default:
-        return router.push('/')
+        return router.push('/');
         break;
     }
-  }
+  };
 
   return (
-  <main>
-    {/* <header className="h-[100px] bg-[#F5F5F5] flex justify-between items-center pl-[100px] pr-[20px]"> */}
-    <header>
-      <nav className="h-[100px] bg-[#F5F5F5] flex justify-between items-center pl-[100px] pr-[20px]">
-        <h1 className="text-[24px] text-black font-bold">Campus Innovate</h1>
-        <section className="flex gap-10 items-center">
-          {navbarContent.map((menu, index) => (
-            <button 
-              onClick={() => handleSelect(index)} 
-              className={` ${pathname === menu.link ? 'text-secondary font-extrabold bg-primary rounded-full py-2 px-3' : 'text-primary'} text-[18px] font-normal hover:text-secondary hover:font-bold`} 
-              key={index}>
-              {menu.title}
-            </button>
-          ))}
-          {/* <button className="py-[14px] px-5 text-[18px] font-medium text-black bg-secondary rounded-lg  hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+    <main className="z-30">
+      {/* <header className="h-[100px] bg-[#F5F5F5] flex justify-between items-center pl-[100px] pr-[20px]"> */}
+      <header>
+        <nav className="h-[100px] bg-[#F5F5F5] flex justify-between items-center pl-[100px] pr-[20px]">
+          <main onClick={() => router.push('/home')} className="flex gap-1 items-center cursor-pointer">
+            {/* <Image alt="logo" src={'/assets/logos/campusinnovate.png'} width={50} height={50} /> */}
+            <h1 className="text-[24px] text-black font-bold">Campus Innovate</h1>
+          </main>
+          <section className="flex gap-10 items-center">
+            {navbarContent.map((menu, index) => (
+              <button
+                onClick={() => handleSelect(index)}
+                className={` ${pathname === menu.link ? 'text-secondary font-extrabold bg-primary rounded-full py-2 px-3' : 'text-primary'} text-[18px] font-normal hover:text-secondary hover:font-bold`}
+                key={index}
+              >
+                {menu.title}
+              </button>
+            ))}
+            {/* <button className="py-[14px] px-5 text-[18px] font-medium text-black bg-secondary rounded-lg  hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
             Book an Appointment
           </button> */}
-          <Button 
-            className='py-[14px] px-5 text-[18px] bg-secondary/90 text-primary border-secondary flex gap-3' 
-            containerClassName='w-[17.5rem]' 
-            borderRadius='1.2rem' 
-            borderClassName='bg-yellow-900 bg-[radial-gradient(var(--primary)_40%,transparent_60%)]'
-          ><FontAwesomeIcon icon={faUmbrellaBeach} fontSize={20} />Book an Appointment</Button>
-        </section>
-      </nav>
-    </header>
-  </main>
+            <Button
+              className="py-[14px] px-5 text-[18px] bg-secondary/90 text-primary border-secondary flex gap-3"
+              containerClassName="w-[17.5rem]"
+              borderRadius="1.2rem"
+              borderClassName="bg-yellow-900 bg-[radial-gradient(var(--primary)_40%,transparent_60%)]"
+            >
+              <FontAwesomeIcon icon={faUmbrellaBeach} fontSize={20} />
+              Book an Appointment
+            </Button>
+          </section>
+        </nav>
+      </header>
+    </main>
   );
 }
